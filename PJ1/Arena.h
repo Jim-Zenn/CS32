f9dfa944539a44e56d72b60d8d0fff02d219842e
Jim Zenn <jimzenn0@gmail.com>
2019-01-13 08:57:53 -0800

    seperate Zombie and Arena declaration

---
+++ PJ1/Arena.h
@@ -0,0 +1,45 @@
+/*
+ * Arena.h
+ * Copyright (C) 2019 Jim Zenn <zenn@ucla.edu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef ARENA_H
+#define ARENA_H
+
+class Player;
+class Zombie;
+
+class Arena
+{
+  public:
+        // Constructor/destructor
+    Arena(int nRows, int nCols);
+    ~Arena();
+
+        // Accessors
+    int     rows() const;
+    int     cols() const;
+    Player* player() const;
+    int     zombieCount() const;
+    int     numZombiesAt(int r, int c) const;
+    bool    determineNewPosition(int& r, int& c, int dir) const;
+    void    display() const;
+
+        // Mutators
+    bool   addZombie(int r, int c);
+    bool   addPlayer(int r, int c);
+    bool   attackZombieAt(int r, int c, int dir);
+    bool   moveZombies();
+
+  private:
+    int     m_rows;
+    int     m_cols;
+    Player* m_player;
+    Zombie* m_zombies[MAXZOMBIES];
+    int     m_nZombies;
+};
+
+
+#endif /* !ARENA_H */
--- PJ1/main.cpp
+++ PJ1/main.cpp
@@ -12,6 +12,8 @@
 #include <cstdlib>
 
 #include "globals.h"
+#include "Zombie.h"
+#include "Arena.h"
 
 using namespace std;
 
@@ -20,34 +22,6 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////
 
 int decodeDirection(char dir);
-
-///////////////////////////////////////////////////////////////////////////
-// Type definitions
-///////////////////////////////////////////////////////////////////////////
-
-class Arena;  // This is needed to let the compiler know that Arena is a
-              // type name, since it's mentioned in the Zombie declaration.
-
-class Zombie
-{
-  public:
-        // Constructor
-    Zombie(Arena* ap, int r, int c);
-
-        // Accessors
-    int  row() const;
-    int  col() const;
-
-        // Mutators
-    void move();
-    bool getAttacked(int dir);
-
-  private:
-    Arena* m_arena;
-    int    m_row;
-    int    m_col;
-    int    m_health;
-};
 
 class Player
 {
@@ -74,36 +48,6 @@ class Player
     bool   m_dead;
 };
 
-class Arena
-{
-  public:
-        // Constructor/destructor
-    Arena(int nRows, int nCols);
-    ~Arena();
-
-        // Accessors
-    int     rows() const;
-    int     cols() const;
-    Player* player() const;
-    int     zombieCount() const;
-    int     numZombiesAt(int r, int c) const;
-    bool    determineNewPosition(int& r, int& c, int dir) const;
-    void    display() const;
-
-        // Mutators
-    bool   addZombie(int r, int c);
-    bool   addPlayer(int r, int c);
-    bool   attackZombieAt(int r, int c, int dir);
-    bool   moveZombies();
-
-  private:
-    int     m_rows;
-    int     m_cols;
-    Player* m_player;
-    Zombie* m_zombies[MAXZOMBIES];
-    int     m_nZombies;
-};
-
 class Game
 {
   public:
@@ -117,59 +61,6 @@ class Game
   private:
     Arena* m_arena;
 };
-
-///////////////////////////////////////////////////////////////////////////
-//  Zombie implementation
-///////////////////////////////////////////////////////////////////////////
-
-Zombie::Zombie(Arena* ap, int r, int c)
-{
-    if (ap == nullptr)
-    {
-        cout << "***** A zombie must be created in some Arena!" << endl;
-        exit(1);
-    }
-    if (r < 1  ||  r > ap->rows()  ||  c < 1  ||  c > ap->cols())
-    {
-        cout << "***** Zombie created with invalid coordinates (" << r << ","
-             << c << ")!" << endl;
-        exit(1);
-    }
-    m_arena = ap;
-    m_row = r;
-    m_col = c;
-    m_health = INITIAL_ZOMBIE_HEALTH;
-}
-
-int Zombie::row() const
-{
-    return m_row;
-}
-
-int Zombie::col() const
-{
-    return m_col;
-}
-
-void Zombie::move()
-{
-      // Attempt to move in a random direction; if we can't move, do not move
-    int dir = randInt(0, NUMDIRS-1);  // dir is now UP, DOWN, LEFT, or RIGHT
-    m_arena->determineNewPosition(m_row, m_col, dir);
-}
-
-bool Zombie::getAttacked(int dir)  // return true if dies
-{
-    m_health--;
-    if (m_health == 0)
-        return true;
-    if ( ! m_arena->determineNewPosition(m_row, m_col, dir))
-    {
-        m_health = 0;
-        return true;
-    }
-    return false;
-}
 
 ///////////////////////////////////////////////////////////////////////////
 //  Player implementations
