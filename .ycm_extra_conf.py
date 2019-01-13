a3fd93ae736148e8aabf4f8cabd88031f3489f3d
Jim Zenn <jimzenn0@gmail.com>
2019-01-13 08:17:35 -0800

    initialize project 1

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,5 @@
+def Settings( **kwargs ):
+  return {
+    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++17', '-isystem', '/data/data/com.termux/files/usr/include/c++/v1' ],
+  }
+
