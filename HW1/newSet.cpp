6d3b6147956447c41444f39ebbb72f316c028b3b
Jim Zenn <jimzenn0@gmail.com>
2019-01-18 19:31:21 -0800

    finish HW1 question 1 - 3

---
+++ HW1/newSet.cpp
@@ -0,0 +1,89 @@
+/*
+ * Set.cpp
+ * Copyright (C) 2019 Jim Zenn <zenn@ucla.edu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+
+#include "newSet.h"
+
+using namespace std;
+
+bool Set::insert(const ItemType &value) {
+    if (contains(value))
+        // the value is already present in the set
+        return false;
+    if (is_full())
+        // the set is already full
+        return false;
+    // find the spot for the new value
+    int i;
+    for (i = 0; i < size(); i += 1) {
+        if (*m_array[i] > value) {
+            // found the spot
+            break;
+        }
+    }
+    // now everybody to the right, move!
+    for (int j = size(); j > i; j -= 1) {
+        m_array[j] = m_array[j-1];
+    }
+    // insert the new item
+    m_array[i] = new ItemType(value);
+    m_size += 1;
+    return true;
+}
+
+bool Set::erase(const ItemType& value) {
+    int i;
+    for (i = 0; i < size(); i += 1) {
+        if (*m_array[i] == value)
+            // found the value
+            break;
+    }
+    if (i == size())
+        // value not present
+        return false;
+    m_size -= 1;
+    for (; i < size(); i += 1) {
+        m_array[i] = m_array[i+1];
+    }
+    return true;
+}
+
+bool Set::contains(const ItemType& value) const {
+    for (int i = 0; i < size(); i += 1) {
+        if (*m_array[i] == value)
+            // found the value
+            return true;
+    }
+    // Not found
+    return false;
+}
+
+bool Set::get(int i, ItemType& value) const {
+    if (i < 0 || i >= size())
+        // out of range
+        return false;
+    value = ItemType(*m_array[i]);
+    return true;
+}
+
+void Set::swap(Set& other) {
+    // swap array
+    ItemType** tmp_ptr = m_array;
+    m_array = other.m_array;
+    other.m_array = tmp_ptr;
+    int tmp;
+    // swap size
+    tmp = m_size;
+    m_size = other.m_size;
+    other.m_size = tmp;
+    // swap capacity
+    tmp = m_capacity;
+    m_capacity = other.m_capacity;
+    other.m_capacity = tmp;
+}
+
---
+++ HW1/newSet.h
@@ -0,0 +1,74 @@
+/*
+ * Set.h
+ * Copyright (C) 2019 Jim Zenn <zenn@ucla.edu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef SET_H
+#define SET_H
+
+#include <string>
+
+const int DEFAULT_MAX_ITEMS = 250;
+
+using ItemType = std::string;
+
+class Set
+{
+    public:
+        Set(const int& capacity = DEFAULT_MAX_ITEMS)
+            : m_capacity(capacity), m_size(0),
+            m_array(new ItemType*[m_capacity]) {};
+          // Create an empty set (i.e., one with no items).
+
+        Set(const Set& orig)
+          // Copy the orig set.
+            : m_capacity(orig.m_capacity), m_size(orig.m_size),
+            m_array(new ItemType*[m_capacity]) {
+                for (int i = 0; i < orig.size(); i ++)
+                    m_array[i] = new ItemType(*orig.m_array[i]);
+        }
+
+        ~Set() { delete [] m_array; };
+          // Destruct the set.
+
+        bool empty() { return m_size == 0; };
+          // Return true if the set is empty, otherwise false.
+
+        int size() const { return m_size; }
+          // Return the number of items in the set.
+
+        bool insert(const ItemType& value);
+          // Insert value into the set if it is not already present.  Return
+          // true if the value was actually inserted.  Leave the set unchanged
+          // and return false if the value was not inserted (perhaps because it
+          // was already in the set or because the set has a fixed capacity and
+          // is full).
+
+        bool erase(const ItemType& value);
+          // Remove the value from the set if present.  Return true if the
+          // value was removed; otherwise, leave the set unchanged and
+          // return false.
+
+        bool contains(const ItemType& value) const;
+          // Return true if the value is in the set, otherwise false.
+
+        bool get(int i, ItemType& value) const;
+          // If 0 <= i < size(), copy into value the item in the set that is
+          // strictly greater than exactly i items in the set and return true.
+          // Otherwise, leave value unchanged and return false.
+
+        void swap(Set& other);
+          // Exchange the contents of this set with the other one.
+
+    private:
+        bool is_full() { return m_size >= m_capacity; };
+            // Return whether the set is full.
+        int m_capacity;
+        int m_size;
+        ItemType **m_array;
+};
+
+#endif /* !SET_H */
+
