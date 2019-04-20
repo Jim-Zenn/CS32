d4feb9417fdf3d62287fd17d9309c3eb1c31fd0a
Jim Zenn <jimzenn0@gmail.com>
2019-04-20 09:31:20 -0700

    remove template and YouCompleteMe configuration

--- .ycm_extra_conf.py
+++
@@ -1,5 +0,0 @@
-def Settings( **kwargs ):
-  return {
-    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++17', '-isystem', '/data/data/com.termux/files/usr/include/c++/v1' ],
-  }
-
